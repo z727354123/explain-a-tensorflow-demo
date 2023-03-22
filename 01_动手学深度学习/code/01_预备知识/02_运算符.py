23326afb92861ff732a898174f372839005fe519
judy <7273654123@qq.com>
2023-03-23 00:03:01 +0800

    补充

--- 01_动手学深度学习/code/01_预备知识/02_运算符.py
+++ 01_动手学深度学习/code/01_预备知识/02_运算符.py
@@ -24,9 +24,7 @@ if __name__ == '__main__':
 		y = torch.tensor([[2.0, 1, 4, 3]])
 		print(x)
 		print(y)
-		print(torch.cat((x,y)))
+		print(torch.cat((x, y), dim=0))
 		# print(torch.cat((x,y), dim=1))
 		# print(torch.cat((x,y), dim=-1))
-		print(torch.cat((x,y), dim=-2))
-
-		
+		print(torch.cat((x, y), dim=-2))
